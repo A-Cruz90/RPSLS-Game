cf12c8d14fb1410910983ccd4aa3c7ad344a2be3
Angel Cruz <cruzangel99@gmail.com>
2022-08-12 10:01:48 -0400

    Created a temp intro to see how the beginning of RPSLS looks like

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import gestures
+# import gestures
 
-
+print("Hello and welcome to RPSLP! Rock, Paper, Scissors, Lizard, Spock. Here are the game rules. ")
+print("Scissors beats Paper, ")
+print("Scissors decapitates Lizard, ")
+print("Rock crushes Scissors, ")
+print("Rock crushes Lizard, ")
+print("Paper covers Rock, ")
+print("Paper disproves Spock, ")
+print("Lizard eats Paper, ")
+print("Lizard poisons Spock, ")
+print("Spock smashes Scissors, ")
+print("and lastly Spock vaporizes Rock. Good Luck. ")
 
--- player.py
+++ player.py
@@ -1,6 +1,14 @@
-
+from hands import list_of_gestures
 
 
 class Player():
     def __init__(self, name):
         self.name = name 
+        self.select_gesture()
+
+
+    def select_gesture(self):
+        print(f'{print(list_of_gestures)}Where as Rock is number 0 and so on ')
+        print(f'Please select a number beween 0 nad 4' )
+        input("# ")
+        
