0977108b8c4e4c57aceeb56045a3d4697caae1a3
Angel Cruz <cruzangel99@gmail.com>
2022-08-14 02:10:43 -0400

    Located issue with code. Cant find solution. Reaching out for assistance.

--- game.py
+++ game.py
@@ -9,20 +9,18 @@ player_two = Player("Billy")
 ai_bot = AI_bot()
 
 def run_game():
-    print("How many players? ")
-    print(input("0, 1, 2?: "))
     while run_game == True:
         print(list_of_gestures)
         player_one.gesture = input(" please select your gesture: ")
         player_two.gesture = input(" Please select your gesture: ")
         ai_bot.getsure = random.choice(list_of_gestures)
 
-        if player_one.gesture > player_two.gesture:
+        if player_one.gesture == player_two.gesture :
+            print(f"Both players chose {player_one.gesture}. Therefore it's a tie! ")
+        elif player_one.gesture > player_two.gesture:
             player_one.win_counter += 1 
         elif player_two.gesture > player_one.gesture:
             player_two.win_counter += 1 
-        elif player_one.gesture == player_two.gesture :
-            print
         elif player_one.win_counter or player_two.win_counter == 2:
             run_game = False
 
